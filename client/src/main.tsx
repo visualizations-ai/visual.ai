0fd2ad33b4482b594130e875940a49b5e3f1f274
Miri Radunsky <miriradunsky@gmail.com>
2025-05-22 15:46:36 +0300

    Add Apollo GraphQL integration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux' 
 import { store } from './store/store';
 import { ApolloProvider } from '@apollo/client';
-import client from './lib/apolloClient';
+import client from './graphql/apollo-client.ts';
 
 import './index.css'
 import App from './App.tsx'
