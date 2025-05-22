4e427b3fdee27ab4d7569708d37359cef3cbc272
Ruchama <r0534104273@gmail.com>
2025-05-22 15:29:46 +0300

    Update main.tsx to support Apollo Client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux' 
 import { store } from './store/store';
+import { ApolloProvider } from '@apollo/client';
+import client from './lib/apolloClient';
 
 import './index.css'
 import App from './App.tsx'
@@ -9,7 +11,9 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </Provider>
   </StrictMode>,
-)
+);
